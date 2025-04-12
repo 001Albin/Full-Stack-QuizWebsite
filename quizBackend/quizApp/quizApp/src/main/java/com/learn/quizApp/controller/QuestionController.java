e92fa8fb13b0f6783203165efcf8f5d20af2926c
Albin Sunny <146367720+001Albin@users.noreply.github.com>
2025-04-12 13:03:29 +0530

    Update QuestionController.java

--- quizBackend/quizApp/quizApp/src/main/java/com/learn/quizApp/controller/QuestionController.java
+++ quizBackend/quizApp/quizApp/src/main/java/com/learn/quizApp/controller/QuestionController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/question")
-@CrossOrigin(origins = "http://localhost:5173/")
+@CrossOrigin(origins = {"http://localhost:5173/", "https://quizforprogrammers.netlify.app/"})
 public class QuestionController {
 
     @Autowired
